114f24eab6cfd53565d44741602a675a60cc30c1
UUAM <UUAM@UUAM-PC>
2014-01-12 16:43:16 +0200

    reply to

--- src/mailclient/core/EmailClient.java
+++ src/mailclient/core/EmailClient.java
@@ -159,7 +159,9 @@ public class EmailClient {
                     message.getSubject(),
                     getContentText(message.getContent()),
                     message.isSet(Flags.Flag.SEEN),
-                    message.getMessageNumber()));
+                    message.getMessageNumber(),
+                    message.getSentDate()
+                    ));
         }
         return result;
     }
